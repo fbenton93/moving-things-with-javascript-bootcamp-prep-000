16df96b5ee9efd605d9cc392dece3b8b2aaeb99b
Forrest Benton <elliottbenton@gmail.com>
2018-07-02 23:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px','')
+  var left = parseInt(leftNumbers)
+  
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+  
+}
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.left.replace('px','')
+  var right = parseInt(rightNumbers)
+  
+  if (right < 360) {
+    dodger.style.left = `${right + 1}px`
+  }
+}
+
+var body = document.body
+body.addEventListener('keydown',function(e) {
+  if (e.which === 39) {
+    moveDodgerRight();
+  }
+  
+  if (e.which === 37) {
+    moveDodgerLeft();
+  }
+})
